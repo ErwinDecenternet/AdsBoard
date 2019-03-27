69bd89f6a073af4ed69efeeaf2309d4a9e8fedf0
Erwin Panganiban <erwin.decenternet.net>
2019-03-27 14:34:23 +0800

    Environment Setup

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\winux\desktop\decenternet\projects\dama\adsboard\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
